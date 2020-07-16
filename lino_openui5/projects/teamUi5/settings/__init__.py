237299d9cdcda308ae7d494f80c98d7a4ab07f6f
Luc Saffre <luc.saffre@gmail.com>
2020-07-16 07:48:50 +0300

    http://luc.lino-framework.org/blog/2020/0716.html

--- lino_openui5/projects/teamUi5/settings/__init__.py
+++ lino_openui5/projects/teamUi5/settings/__init__.py
@@ -1,31 +1,29 @@
 # -*- coding: UTF-8 -*-
 # Copyright 2015-2018 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
-"""The :xfile:`settings.py` modules for this variant.
 
-It has :attr:`default_user<lino.core.site.Site.default_user>` set to
-'anonymous', which causes it to deactivate both authentication and
-sessions.
+import datetime
 
-.. autosummary::
-   :toctree:
-
-   demo
-   doctests
-
-"""
-
-
-from lino_book.projects.team.settings import *
+from lino_noi.lib.noi.settings import *
 
 
 class Site(Site):
-    pass
+
     default_ui = 'lino_openui5.openui5'
     project_name = "openui5_teamUi5"
-    title = "Team Lino Open Ui5 demo"
+    title = "Lino Noi Open Ui5 demo"
 
-    def get_installed_apps(self):
-        yield super(Site, self).get_installed_apps()
-        # Add the stars plugin because it gives a repeatable action for testing.
-        # yield 'lino_xl.lib.stars'
+    the_demo_date = datetime.date(2020, 5, 23)
+
+    languages = "en de fr"
+    # readonly = True
+    # catch_layout_exceptions = False
+
+    # use_linod = True
+    # use_ipdict = True
+    # use_websockets = True
+    # use_experimental_features = True
+    # default_ui = 'lino_extjs6.extjs6'
+    # default_ui = 'lino.modlib.bootstrap3'
+    # default_ui = 'lino_openui5.openui5'
+    # default_ui = 'lino_react.react'
